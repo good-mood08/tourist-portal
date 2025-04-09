e837d63f55f18b03552b4e2c14e43ffe0ddf43cd
ekmakar <liz.ist.15@bk.ru>
2025-04-09 11:17:32 +0300

    компоненты шрифтов начало

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
-  
+    <p class="font-(family-name:Grotesk) text-[14px]">d,nsjfhjke</p>
+    <p class="fo">d,nsjfhjke</p>
+    <p class="fo1">d,nsjfhjke</p>
+    <p class="fo2">d,nsjfhjke</p>
   </div>
 </template>
+
+<style>
+.fo {
+  font-family: 'Grotesk';
+}
+.fo1 {
+  font-family: 'GroteskWideM';
+}
+.fo2 {
+  font-family: 'GroteskWideR';
+}
+</style>
