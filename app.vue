78507a65b246702098853965075acbdd47f82765
ekmakar <liz.ist.15@bk.ru>
2025-04-09 13:11:45 +0300

    g

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p class="font-(family-name:Grotesk) text-[14px]">d,nsjfhjke</p>
+    <p class="font-(family-name:Grotesk) text-[74px]">d,nsjfhjke</p>
     <p class="fo">d,nsjfhjke</p>
     <p class="fo1">d,nsjfhjke</p>
     <p class="fo2">d,nsjfhjke</p>
